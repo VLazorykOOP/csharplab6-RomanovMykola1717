dfe6549bf782e91e1cf6fae0d99a003f6516c9f5
RomanovMykola1717 <162117283+RomanovMykola1717@users.noreply.github.com>
2024-06-07 07:59:57 +0300

    Add files via upload

--- Lab6CSharp/Program.cs
+++ Lab6CSharp/Program.cs
@@ -1,58 +1,118 @@
-﻿// See https://aka.ms/new-console-template for more information
-/// <summary>
-///  Top-level statements 
-///  Код програми (оператори)  вищого рівня
-/// </summary>
-///
-Console.WriteLine("Lab6 C# ");
-AnyFunc();
+﻿using System;
 
-/// <summary>
-/// 
-///  Top-level statements must precede namespace and type declarations.
-/// At the top-level methods/functions can be defined and used
-/// На верхньому рівні можна визначати та використовувати методи/функції
-/// </summary>
-void AnyFunc()
+public interface IProgrammableSoftware
 {
-    Console.WriteLine(" Some function in top-level");
+    string Name { get; set; }
+    string Manufacturer { get; set; }
+
+    void DisplayInfo();
+    bool IsUsable(DateTime currentDate);
 }
-Console.WriteLine("Problems 1 ");
-AnyFunc();
-//  приклад класів
-UserClass cl = new UserClass();
-cl.Name = " UserClass top-level ";
-User.UserClass cl2 = new();
-cl2.Name = " UserClass namespace User ";
 
-
-
-
-/// <summary>
-/// 
-/// Top-level statements must precede namespace and type declarations.
-/// Оператори верхнього рівня мають передувати оголошенням простору імен і типу.
-/// Створення класу(ів) або оголошенням простору імен є закіченням  іструкцій верхнього рівня
-/// 
-/// </summary>
-
-namespace User
-{
-    class UserClass
-    {
-        public string Name { get; set; }
-        public UserClass()
-        {
-            Name = "NoName";
-        }
-        UserClass(string n)
-        {
-            Name = n;
-        }
-    }
-
-}
-class UserClass
+public class FreeSoftware : IProgrammableSoftware
 {
     public string Name { get; set; }
+    public string Manufacturer { get; set; }
+
+    public FreeSoftware(string name, string manufacturer)
+    {
+        Name = name;
+        Manufacturer = manufacturer;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Free Software: {Name}, Manufacturer: {Manufacturer}");
+    }
+
+    public bool IsUsable(DateTime currentDate)
+    {
+        // Free software is always usable
+        return true;
+    }
+}
+
+public class Shareware : IProgrammableSoftware
+{
+    public string Name { get; set; }
+    public string Manufacturer { get; set; }
+    public DateTime InstallationDate { get; set; }
+    public int FreeUsagePeriod { get; set; } // in days
+
+    public Shareware(string name, string manufacturer, DateTime installationDate, int freeUsagePeriod)
+    {
+        Name = name;
+        Manufacturer = manufacturer;
+        InstallationDate = installationDate;
+        FreeUsagePeriod = freeUsagePeriod;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Shareware: {Name}, Manufacturer: {Manufacturer}, Installation Date: {InstallationDate.ToShortDateString()}, Free Usage Period: {FreeUsagePeriod} days");
+    }
+
+    public bool IsUsable(DateTime currentDate)
+    {
+        return (currentDate - InstallationDate).TotalDays <= FreeUsagePeriod;
+    }
+}
+
+public class CommercialSoftware : IProgrammableSoftware
+{
+    public string Name { get; set; }
+    public string Manufacturer { get; set; }
+    public decimal Price { get; set; }
+    public DateTime InstallationDate { get; set; }
+    public int UsagePeriod { get; set; } // in days
+
+    public CommercialSoftware(string name, string manufacturer, decimal price, DateTime installationDate, int usagePeriod)
+    {
+        Name = name;
+        Manufacturer = manufacturer;
+        Price = price;
+        InstallationDate = installationDate;
+        UsagePeriod = usagePeriod;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Commercial Software: {Name}, Manufacturer: {Manufacturer}, Price: {Price:C}, Installation Date: {InstallationDate.ToShortDateString()}, Usage Period: {UsagePeriod} days");
+    }
+
+    public bool IsUsable(DateTime currentDate)
+    {
+        return (currentDate - InstallationDate).TotalDays <= UsagePeriod;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Створення масиву програмного забезпечення
+        IProgrammableSoftware[] softwareArray = new IProgrammableSoftware[]
+        {
+            new FreeSoftware("Linux", "Open Source Community"),
+            new Shareware("WinRAR", "RARLAB", new DateTime(2024, 1, 1), 40),
+            new CommercialSoftware("Microsoft Office", "Microsoft", 149.99m, new DateTime(2023, 6, 1), 365)
+        };
+
+        // Виведення інформації про все програмне забезпечення
+        foreach (var software in softwareArray)
+        {
+            software.DisplayInfo();
+            Console.WriteLine($"Usable: {software.IsUsable(DateTime.Now)}\n");
+        }
+
+        // Пошук програмного забезпечення, яке припустимо використовувати на поточну дату
+        Console.WriteLine("Search for usable software on the current date:");
+        foreach (var software in softwareArray)
+        {
+            if (software.IsUsable(DateTime.Now))
+            {
+                software.DisplayInfo();
+            }
+        }
+    }
 }
